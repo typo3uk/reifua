4c13ac4cc4eb5ae0ab06323ce89015f6c5c8f61c
TYPO3 Integrator <typo3@ukr.net>
2025-09-12 11:13:00 +0000

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['www.realestate.if.ua'],
+  },
 };
 
 export default nextConfig;
